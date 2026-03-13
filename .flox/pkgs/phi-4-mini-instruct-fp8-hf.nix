97c7ba8b76fd3aea4eea9360b77730309ac96c85
barstoolbluz <steve@flox.dev>
2026-03-13 11:27:27 -0400

    refactored package we use to publish phi-4-mini-instruct
    Signed-off-by: barstoolbluz <steve@flox.dev>

--- .flox/pkgs/phi-4-mini-instruct-fp8-hf.nix
+++ .flox/pkgs/phi-4-mini-instruct-fp8-hf.nix
@@ -1,9 +1,9 @@
-# Phi-4-mini-instruct FP8-TORCHAO for vLLM
+# Phi-4-mini-instruct FP8-TORCHAO (HuggingFace format)
 { pkgs, mkHfModel ? pkgs.callPackage ./mkHfModel.nix {} }:
 
 mkHfModel {
-  pname = "vllm-phi-4-mini-instruct-fp8";
-  version = "1.0.0";
+  pname = "phi-4-mini-instruct-fp8-hf";
+  version = "1.0.1";
   srcPath = /mnt/scratch/models/inferencing/hub/models--microsoft--Phi-4-mini-instruct-FP8-TORCHAO/snapshots/b63ecd840bb9835f35e6d884d47810c4deec89dc;
   slug = "microsoft--Phi-4-mini-instruct-FP8-TORCHAO";
   snapshotId = "b63ecd840bb9835f35e6d884d47810c4deec89dc";
